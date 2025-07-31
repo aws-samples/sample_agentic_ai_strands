74106a151bc936e2bc70258d6f2c7985bfa6c655
River Xie <xiehust@163.com>
2025-07-31 23:31:26 +0800

    update

--- src/chat_client.py
+++ src/chat_client.py
@@ -46,11 +46,12 @@ class ChatClient:
         """clear session message of this client"""
         self.messages = []
         self.system = None
+        self.agent = None
         # 如果是其他类型的multiagent则直接设置成空
-        if not isinstance(self.agent,Agent):
-            self.agent = None
-        elif self.agent:
-            self.agent.messagas = []
+        # if not isinstance(self.agent,Agent):
+        #     self.agent = None
+        # elif self.agent:
+        #     self.agent.messagas = []
         if self.memory_provider:
             self.memory_provider.delete_all_events(self.user_id)
     
